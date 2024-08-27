3b829b3b5c8755fb5489a356015fd92fdb1ff5c8
Juan <juanpanizzi@hotmail.com>
2024-08-27 12:01:32 -0300

    advences on responsive views

--- src/componentes/Header.tsx
+++ src/componentes/Header.tsx
@@ -22,8 +22,8 @@ export const Header = () => {
     className="w-full h-full object-cover transform scale-130 object-[calc(100%+12.5rem)_center] sm:object-[calc(100%+8rem)_center] lg:object-[calc(100%+5rem)_center] sm:scale-100 lg:scale-140 sm:object-center lg:object-center "
   />
   <div className="absolute inset-0 bg-gradient-to-t from-azul to-transparent"></div>
-  <div className="absolute top-48 lg:top-42 left-4 lg:left-8 flex flex-col text-white items-start  z-10 " >
-    <h1 className="text-3xl sm:text-5xl lg:text-6xl xl:text-7xl font-inter leading-snug sm:leading-normal  font-bold " style={{ textShadow: '2px 2px 10px black' }}>Transporte <span className='block sm:inline'> seguro </span>  <span className='sm:block'>y eficiente</span></h1>
+  <div className="absolute top-48 lg:top-42 xl:top-36 left-4 lg:left-8 flex flex-col text-white items-start  z-10 " >
+    <h1 className="text-3xl sm:text-5xl lg:text-6xl xl:text-7xl font-inter leading-snug sm:leading-normal xl:leading-normal font-bold " style={{ textShadow: '2px 2px 10px black' }}>Transporte <span className='block sm:inline'> seguro </span>  <span className='sm:block'>y eficiente</span></h1>
     <p className="text-sm sm:text-lg w-2/3 mt-5 ">Tenemos más de 25 años de experiencia. Somos una pyme, y nuestra flota cuenta con con chasis y semis tipo saider. Llevamos volumen y poco peso. Contamos con chasis de 5 y 10 mil kilos y semis de 15 y 22 mil kilos.</p>
 
   </div>
