38137a7c2f3f2b578faa3e1090973b1dce434718
Juan <juanpanizzi@hotmail.com>
2024-08-27 15:32:12 -0300

    advances on adding stats

--- src/componentes/Stats.tsx
+++ src/componentes/Stats.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Stats = () => {
   return (
-    <div className='h-80 bg-azul w-full flex justify-evenly items-center'>
+    <div className='h-80 bg-azul w-full flex justify-center items-center'>
 
-      <div className="flex flex-col items-center w-1/3 p-2">
+      <div className="flex flex-col items-center w-1/8  p-2">
 
         <div className="flex items-center justify-center w-44 h-44 rounded-full border-2 border-white text-white text-4xl font-bold">
           +5000
@@ -13,7 +13,7 @@ export const Stats = () => {
       </div>
 
 
-      <div className="flex flex-col items-center w-1/3 p-2">
+      <div className="flex flex-col items-center w-1/8  p-2 mx-20">
 
         <div className="flex items-center justify-center w-44 h-44 rounded-full border-2 border-white text-white text-4xl font-bold">
           +50.000
@@ -23,7 +23,7 @@ export const Stats = () => {
 
 
 
-      <div className="flex flex-col items-center w-1/3 p-2">
+      <div className="flex flex-col items-center w-1/8  p-2">
 
         <div className="flex items-center justify-center w-44 h-44 rounded-full border-2 border-white text-white text-4xl font-bold">
           +25
