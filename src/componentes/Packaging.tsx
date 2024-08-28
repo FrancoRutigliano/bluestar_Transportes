2da34f132baa909c6903cb93ce05d69c768e691d
Juan <juanpanizzi@hotmail.com>
2024-08-28 16:34:16 -0300

    Packaging desktop view done

--- src/componentes/Packaging.tsx
+++ src/componentes/Packaging.tsx
@@ -2,13 +2,13 @@ import paqueteria from '../assets/paqueteria.jpg'
 
 export const Packaging = () => {
   return (
-    <section className="h-screen bg-sky-200 flex">
+    <section className="h-screen  flex">
         
-        <div className='bg-red-200 w-1/2 flex justify-center items-center px-10'>
+        <div className=' w-1/2 flex justify-center items-center px-10'>
             <img src={paqueteria} alt="" className='' />
         </div>
-        <div className='bg-sky-500 w-1/2 flex items-center '>
-        <div className='bg-green-200 py-32 border-l-4 border-azul flex items-center '>
+        <div className=' w-1/2 flex items-center '>
+        <div className='   py-32 border-l-4 border-azul flex items-center '>
             <p className='text-azul  lg:text-3xl ml-10 mr-20'>Tenemos amplia experiencia con plastiqueras y empresas que realizan packaging industrial</p>
         </div>
         </div>
