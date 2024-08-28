ad6701a17f5ee76e148be060cc06650804497375
Juan <juanpanizzi@hotmail.com>
2024-08-28 16:12:44 -0300

    advances con packagin

--- src/componentes/Packaging.tsx
+++ src/componentes/Packaging.tsx
@@ -8,8 +8,8 @@ export const Packaging = () => {
             <img src={paqueteria} alt="" className='' />
         </div>
         <div className='bg-sky-500 w-1/2 flex items-center '>
-        <div className='bg-green-200 h-2/3 border-l-4 border-azul'>
-            <p className='text-azul'>Tenemos amplia experiencia con plastiqueras y empresas que realizan packaging industrial</p>
+        <div className='bg-green-200 py-32 border-l-4 border-azul flex items-center '>
+            <p className='text-azul  lg:text-3xl ml-10 mr-20'>Tenemos amplia experiencia con plastiqueras y empresas que realizan packaging industrial</p>
         </div>
         </div>
 
