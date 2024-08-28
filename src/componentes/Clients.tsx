29dee5f0d6f940770d2b9c092b17b25e5cb16867
Juan <juanpanizzi@hotmail.com>
2024-08-28 11:14:53 -0300

    advances on marquee

--- src/componentes/Clients.tsx
+++ src/componentes/Clients.tsx
@@ -11,27 +11,26 @@ export const Clients = () => {
 
     <section className="h-80  mx-20">
 
-    <h1 className="text-center text-6xl mt-10">Nuestros Clientes</h1>
+    <h1 className="text-center text-5xl mt-20 font-medium ">Nuestros Clientes</h1>
 
-    <div className="relative overflow-hidden h-62">
-      {/* <div className="flex space-x-8"> */}
-    <Marquee speed={150}>
-        <img src={iconplast} alt="" className="h-60 w-60 object-contain" />
-        <img src={altec} alt="" className="h-60 w-60 object-contain" />
-        <img src={ypf} alt="" className="h-60 w-60 object-contain" />
-        <img src={endonsan} alt="" className="h-60 w-60 object-contain" />
-        <img src={plastinhort} alt="" className="h-60 w-60 object-contain" />
-        <img src={grief} alt="" className="h-60 w-60 object-contain" />
+    <div className="relative overflow-hidden h-62  mt-16 ">
+    <Marquee speed={150} >
+      
+        <img src={iconplast} alt="" className="h-56 w-56 object-contain" />
+        <img src={plastinhort} alt="" className="h-56 w-56 object-contain" />
+        <img src={grief} alt="" className="h-56 w-56 object-contain" />
+        <img src={altec} alt="" className="h-56 w-56 object-contain" />
+        <img src={ypf} alt="" className="h-56 w-56 object-contain p-5" />
+        <img src={endonsan} alt="" className="h-56 w-56 object-contain p-2" />
         {/* Repetimos las imágenes para asegurar la animación continua */}
-        <img src={iconplast} alt="" className="h-60 w-60 object-contain" />
-        <img src={altec} alt="" className="h-60 w-60 object-contain" />
-        <img src={ypf} alt="" className="h-60 w-60 object-contain" />
-        <img src={endonsan} alt="" className="h-60 w-60 object-contain" />
-        <img src={plastinhort} alt="" className="h-60 w-60 object-contain" />
-        <img src={grief} alt="" className="h-60 w-60 object-contain" />
+        <img src={iconplast} alt="" className="h-56 w-56 object-contain" />
+        <img src={plastinhort} alt="" className="h-56 w-56 object-contain" />
+        <img src={grief} alt="" className="h-56 w-56 object-contain" />
+        <img src={altec} alt="" className="h-56 w-56 object-contain" />
+        <img src={ypf} alt="" className="h-56 w-56 object-contain  p-5" />
+        <img src={endonsan} alt="" className="h-56 w-56 object-contain p-2" />
     </Marquee>
 
-      {/* </div> */}
     </div>
     </section>
   );
