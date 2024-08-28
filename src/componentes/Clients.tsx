2bdcf0e9c4439e2442d714fd39c52367ec863ff1
Juan <juanpanizzi@hotmail.com>
2024-08-28 11:33:21 -0300

    responsive styles on marquee

--- src/componentes/Clients.tsx
+++ src/componentes/Clients.tsx
@@ -9,26 +9,26 @@ import Marquee from "react-fast-marquee";
 export const Clients = () => {
   return (
 
-    <section className="h-80  mx-20">
+    <section className="h-80 mx-10 md:mx-16 lg:mx-20">
 
-    <h1 className="text-center text-5xl mt-20 font-medium ">Nuestros Clientes</h1>
+    <h1 className="text-center text-3xl md:text-4xl lg:text-5xl  mt-20 font-medium ">Nuestros Clientes</h1>
 
     <div className="relative overflow-hidden h-62  mt-16 ">
     <Marquee speed={150} >
       
-        <img src={iconplast} alt="" className="h-56 w-56 object-contain" />
-        <img src={plastinhort} alt="" className="h-56 w-56 object-contain" />
-        <img src={grief} alt="" className="h-56 w-56 object-contain" />
-        <img src={altec} alt="" className="h-56 w-56 object-contain" />
-        <img src={ypf} alt="" className="h-56 w-56 object-contain p-5" />
-        <img src={endonsan} alt="" className="h-56 w-56 object-contain p-2" />
+        <img src={iconplast} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain" />
+        <img src={plastinhort} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain" />
+        <img src={grief} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain" />
+        <img src={altec} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain" />
+        <img src={ypf} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain p-5" />
+        <img src={endonsan} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain p-2" />
         {/* Repetimos las imágenes para asegurar la animación continua */}
-        <img src={iconplast} alt="" className="h-56 w-56 object-contain" />
-        <img src={plastinhort} alt="" className="h-56 w-56 object-contain" />
-        <img src={grief} alt="" className="h-56 w-56 object-contain" />
-        <img src={altec} alt="" className="h-56 w-56 object-contain" />
-        <img src={ypf} alt="" className="h-56 w-56 object-contain  p-5" />
-        <img src={endonsan} alt="" className="h-56 w-56 object-contain p-2" />
+        <img src={iconplast} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain" />
+        <img src={plastinhort} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain" />
+        <img src={grief} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain" />
+        <img src={altec} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain" />
+        <img src={ypf} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain  p-5" />
+        <img src={endonsan} alt="" className="h-44 w-44 md:h-48 md:w-48 lg:h-56 lg:w-56 object-contain p-2" />
     </Marquee>
 
     </div>
