c60aa0fa53e9d2d26c980b2995d03ce9f4840827
Juan <juanpanizzi@hotmail.com>
2024-09-07 13:05:24 -0300

    packaging section

--- src/componentes/Packaging.tsx
+++ src/componentes/Packaging.tsx
@@ -1,15 +1,15 @@
-import paqueteria from '../assets/paqueteria.jpg'
+import montacarga from '../assets/montacarga.svg'
 
 export const Packaging = () => {
   return (
     <section className="h-screen flex">
         
         <div className=' w-1/2 flex justify-center items-center px-10'>
-            <img src={paqueteria} alt="" className='' />
+            <img src={montacarga} alt="" className='' />
         </div>
         <div className=' w-1/2 flex items-center '>
         <div className='   py-32 border-l-4 border-azul flex items-center '>
-            <p className='text-azul  lg:text-3xl ml-10 mr-20'>Tenemos amplia experiencia con plastiqueras y empresas que realizan packaging industrial</p>
+            <p className='text-azul  lg:text-3xl ml-10 mr-20'>Tenemos amplia experiencia con plastiqueras y empresas que realizan packaging industrial.</p>
         </div>
         </div>
 
